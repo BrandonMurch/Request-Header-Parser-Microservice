cfd557b898b1f230a7fdd3fd6fb1fffca03acad9
Brandon Murch <brandon.murch@gmail.com>
2018-04-17 13:58:59 +1000

    Initial commit

---
+++ Build/UserAgent.js
@@ -0,0 +1,5 @@
+window.onload = function() {
+  console.log(JSON.stringify(document.getElementById('UserAgent')));
+  console.log(navigator.userAgent);
+  console.log(navigator.geolocation);
+}
---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+//const geolocation = require('geolocation')
+const app = express();
+
+//app.use(express.static(__dirname + '/build'));
+
+const logIP = (remote, header, req) => {
+  return {
+    'remote' : remote,
+    'header' : header,
+    'req' : req,
+    'test' : 'test'
+  }
+}
+
+app.get('/', (req, res) => {
+  let response = logIP(req.connection.remoteAddress, req.headers['x-forwarded-for'], req.ip)
+  //res.sendFile(path.join(__dirname + '/build/index.html'));
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify(response));
+})
+
+app.listen(3000);
