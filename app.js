b4e5d64a57ec94e3147626457726cf994aa98546
Brandon Murch <brandon.murch@gmail.com>
2018-04-20 12:48:53 +1000

    Added Test

--- app.js
+++ app.js
@@ -3,22 +3,31 @@ const path = require('path');
 const PORT = process.env.PORT || 5000
 const app = express();
 
-const logIP = (header, language, userAgent) => {
-  // ddd.ddd.ddd.ddd
-  const ipRegExp = /(\d){1,3}\.(\d){1,3}\.(\d){1,3}\.(\d){1,3}/gi
-  //en-us
-  const langRegExp = /^([A-z\-])+/gi
-  let ip = ipRegExp.exec(header);
-  let lang = langRegExp.exec(language);
-  return {
-    'IP': ip[0],
-    'Language': lang[0],
-    'User Agent': userAgent
+module.exports.parseHeader = (header, language, userAgent) => {
+    // ddd.ddd.ddd.ddd
+    const ipRegExp = /(\d){1,3}\.(\d){1,3}\.(\d){1,3}\.(\d){1,3}/gi
+    //en-us
+    const langRegExp = /^([A-z\-])+/gi
+    let ip = ipRegExp.exec(header);
+    let lang = langRegExp.exec(language);
+    if (ip){
+      return {
+        'IP': ip[0],
+        'Language': lang[0],
+        'User Agent': userAgent
+      }
+    }else{
+      return {
+        'IP': 'Running server locally on port: '+PORT,
+        'Language': lang[0],
+        'User Agent': userAgent
+      }
+    }
   }
-}
+
 
 app.get('/', (req, res) => {
-  let response = logIP(req.headers['x-forwarded-for'], req.headers['accept-language'], req.headers['user-agent'])
+  let response = this.parseHeader(req.headers['x-forwarded-for'], req.headers['accept-language'], req.headers['user-agent'])
   res.setHeader('Content-Type', 'application/json');
   res.send(JSON.stringify(response));
 })
---
+++ test/test.js
@@ -0,0 +1,30 @@
+const chai = require('chai');
+const assert = chai.assert;
+const request = require('request');
+const app = require('../app.js');
+const parseHeader = app.parseHeader;
+
+describe('RegExp', () => {
+  let header = parseHeader('183.84.8.383l;akjiek3333kfj9', 'en-us:4850P=03', 'mozilla');
+  let expected = {'IP': '183.84.8.383',
+  'Language': 'en-us',
+  'User Agent': 'mozilla'};
+  it('should return parsed IP', () => {
+    assert.equal(header['IP'], expected['IP'], 'Ip is parsed incorrectly');
+  });
+  it('should return parsed Language', () => {
+    assert.equal(header['Language'], expected['Language'], 'Language is parsed incorrectly');
+  });
+  it('should return passed User Agent', () => {
+    assert.equal(header['User Agent'], expected['User Agent'], 'User Agent is passed incorrectly');
+  });
+});
+
+describe('Route', () => {
+  it('should return 200 OK for /home', (done) => {
+    request('http://127.0.0.1:5000', (error, response, body) => {
+      assert.equal(response.statusCode, 200);
+      done();
+    });
+  });
+});
